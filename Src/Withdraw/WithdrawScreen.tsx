5c103c937223b89ca4d600c67501d68b47e874e8
sasi12781a <sasi.chimmili@xcubelabs.com>
2023-02-07 10:17:48 +0530

    m

--- Src/Withdraw/WithdrawScreen.tsx
+++ Src/Withdraw/WithdrawScreen.tsx
@@ -42,7 +42,7 @@ const WithdrawScreen=({ route, navigation })=>{
 
 
     return(
-      <ScrollView >
+      <ScrollView>
         <View style={{justifyContent:'flex-start',alignItems:'center',backgroundColor:"#CBC3E3",height:Height,width:Width}}>
           <View style={{marginHorizontal:Width/30,marginVertical:Width/10}}>
             <Text style={{fontSize:0.02*Height,alignSelf:'flex-start',color:'black'}}>
@@ -56,14 +56,14 @@ const WithdrawScreen=({ route, navigation })=>{
                 value={`${parseInt(goldPercent)>=101 ? parseInt(goldPercent.slice(0,2))/100*goldContain:parseInt(goldPercent)<100 ? goldPercent.slice(0,2): goldPercent.slice(0,3)}`}
                 placeholder={goldPercent || amount ? amount : goldPercent}
             />
-            <View style={{flexDirection:'row',justifyContent:'space-between',marginVertical:Width/50}}>
+            <View style={{flexDirection:'row',justifyContent:'space-between',marginBottom:Width/50}}>
               {percentage.map((val,id)=>{
                 return(
                   <TouchableOpacity key={id} style={{height:0.04*Height,width:0.14*Width,backgroundColor: '#7E8274',borderRadius:0.016*Height,justifyContent:"center"}} onPress={()=>{setGoldPercent(val)}}>
                     <Text style={{fontSize:0.02*Height,color:"black",alignSelf:"center"}}>{`${val}`}</Text>
                   </TouchableOpacity>
                 )
-            })}
+              })}
             </View>
             <Text style={{fontSize:0.02*Height,alignSelf:'flex-start',color:'black',marginTop:Width/15}}>
                 Amount
@@ -77,46 +77,48 @@ const WithdrawScreen=({ route, navigation })=>{
                 placeholder={amount || goldPercent ? goldToSell:amount}
             />
             {(parseInt(goldPercent)>0 || parseInt(amount)>0) ??  (goldPercent || amount) ? 
-            <View style={{backgroundColor:'#7E8274',borderBottomLeftRadius:Width/20,borderBottomRightRadius:Width/20}}>
+            <View style={{backgroundColor:"#7E8274",borderBottomLeftRadius:Width/50,borderBottomRightRadius:Width/50,marginBottom:Width/60}}>
               <Text style={{fontSize:0.02*Height,margin:0.02*Height,justifyContent:'center',alignSelf:'center',fontWeight:'bold',color:"white"}}>
                 Breakdown
               </Text>
-              <View style={{borderBottomColor: 'black',borderBottomWidth: 0.002*Height,marginVertical:Width/60}}/>
-              <View style={{flexDirection:'row',justifyContent:'space-between',marginVertical:Width/50,marginHorizontal:Width/60}}>
-                <Text style={{marginHorizontal:10,color:"white",fontSize:0.02*Height}}>
-                  Gold Quantity
-                </Text>
-                <Text style={{color:"white",fontSize:0.02*Height}}>
-                {`${7} gm`}
-                </Text>
-              </View>
-              <View style={{flexDirection:'row',justifyContent:'space-between',marginVertical:Width/50,marginHorizontal:Width/60}}>
-                <Text style={{marginHorizontal:10,color:"white",fontSize:0.02*Height}}>
-                  Gold Value
-                </Text>
-                <Text style={{color:"white",fontSize:0.02*Height}}>
-                {`${8} gm`}
-                </Text>
-              </View>
-              <View style={{flexDirection:'row',justifyContent:'space-between',marginVertical:Width/50,marginHorizontal:Width/60}}>
-                <Text style={{marginHorizontal:10,color:"white",fontSize:0.02*Height}}>
+              <View style={{borderBottomColor: 'black',borderBottomWidth: 0.002*Height,marginVertical:0.02*Height,width:0.76*Width,marginLeft:0.02*Width}}/>
+              <View>
+                <View style={{flexDirection:'row',justifyContent:"space-between",margin:0.005*Height}}>
+                  <Text style={{paddingHorizontal:Width/70,color:"white",fontSize:0.02*Height}}>
+                    Gold Quantity
+                  </Text>
+                  <Text style={{color:"white",fontSize:0.02*Height,paddingHorizontal:Width/70}}>
+                    {`${8} gm`}
+                  </Text>
+                </View>
+                <View style={{flexDirection:'row',justifyContent:"space-between",margin:0.005*Height}}>
+                  <Text style={{paddingHorizontal:Width/70,color:"white",fontSize:0.02*Height}}>
+                    Gold Value
+                  </Text>
+                  <Text style={{color:"white",fontSize:0.02*Height,paddingHorizontal:Width/70}}>
+                    {`${6} Rs`}
+                  </Text>
+                </View>
+                <View style={{flexDirection:'row',justifyContent:"space-between",margin:0.005*Height}}>
+                  <Text style={{paddingHorizontal:Width/70,color:"white",fontSize:0.02*Height}}>
                     GST
-                </Text>
-                <Text style={{color:"white",fontSize:0.02*Height}}>
-                {`${8} gm`}
-                </Text>
-              </View>
-              <View style={{borderBottomColor: 'black',borderBottomWidth: 0.002*Height,marginVertical:Width/60,width:0.70*Width,alignSelf:"center"}}/>         
-              <View style={{flexDirection:'row',justifyContent:'space-between',marginVertical:Width/50,marginHorizontal:Width/60}}>
-                <Text style={{marginHorizontal:10,color:"white",fontSize:0.02*Height}}>
+                  </Text>
+                  <Text style={{color:"white",fontSize:0.02*Height,paddingHorizontal:Width/70}}>
+                    3 %
+                  </Text>
+                </View>
+                <View style={{borderBottomColor: 'black',borderBottomWidth: 0.002*Height,marginVertical:0.02*Height,width:Width*0.76,marginLeft:0.02*Width}}/>
+                <View style={{flexDirection:'row',justifyContent:"space-between",marginBottom:Width/80}}>
+                  <Text style={{paddingHorizontal:Width/60,color:"white",fontSize:0.02*Height}}>
                     Payable Amount
-                </Text>
-                <Text style={{color:"white",fontSize:0.02*Height}}>
-                {`${8} gm`}
-                </Text>
+                  </Text>
+                  <Text style={{color:"white",fontSize:0.02*Height,paddingHorizontal:Width/60}}>
+                    {`${5}`}
+                  </Text>
+                </View>
               </View>
             </View>:null}
-          </View>
+         </View>
           <TouchableOpacity style={styles.button} onPress={()=>{alertScreen()}}>
               <Text style={{color:"black",fontSize:0.04*Height,alignSelf:'center'}} >
               Withdraw
@@ -149,6 +151,7 @@ const styles = StyleSheet.create({
       position:'absolute',
       bottom:0,
       justifyContent:'center',
+      marginTop:Width/90
     }
 });
    
